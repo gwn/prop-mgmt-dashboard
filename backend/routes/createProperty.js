e3ed077db58c1d08796cd72865b3bbbd01720066
gwn <egeavunc@gmail.com>
2026-01-25 04:29:18 +0300

    Refactor upsert related backend modules

--- backend/routes/createProperty.js
+++ backend/routes/createProperty.js
@@ -1,41 +1,102 @@
 const
     {getDB} = require('../db'),
-    upsertProperty = require('../upsertProperty'),
-    {PropertySchema} = require('../../schema')
+    {PropertySchema} = require('../../schema'),
 
 
-module.exports = {
-    url: '/properties',
-    method: 'post',
+    generateUpsertHandler = (insert = true) => ({
+        url: insert ? '/properties' : '/properties/:id',
+        method: insert ? 'post' : 'put',
 
-    schema: {
-        summary: 'Create property',
-        body: PropertySchema,
-        response: {
-            201: {
-                type: 'object',
-                properties: {
-                    id: {type: 'integer'},
-                    property_manager_id: {type: 'integer'},
-                    accountant_id: {type: 'integer'},
+        schema: {
+            summary: (insert ? 'Create' : 'Update') + ' property',
+            body: PropertySchema,
+            response: {
+                [insert ? 201 : 200]: {
+                    type: 'object',
+                    properties: {
+                        id: {type: 'integer'},
+                        property_manager_id: {type: 'integer'},
+                        accountant_id: {type: 'integer'},
+                    },
                 },
             },
         },
-    },
 
-    handler: async (req, rep) => {
-        const db = await getDB()
+        handler: async (req, rep) => {
+            const db = await getDB()
 
-        try {
-            const propRec = await upsertProperty(db, req.body)
+            try {
+                const propRec =
+                    await upsertProperty(
+                        db,
+                        insert
+                            ? req.body
+                            : {id: req.params.id, ...req.body},
+                    )
 
-            rep.status(201).send(propRec)
+                rep.status(insert ? 201 : 200).send(propRec)
 
-        } catch (e) {
-            if (e.code === '23505')
-                rep.status(409).send()
-            else
-                throw e
-        }
-    },
+            } catch (e) {
+                if (e.code === '23505')
+                    rep.status(409).send()
+                else
+                    throw e
+            }
+        },
+    }),
+
+
+    upsertProperty = (
+        db,
+        {
+            property_manager,
+            accountant,
+            declaration_file,
+            buildings,
+            ...prop
+        },
+    ) =>
+        db.withTransaction(async tx => {
+            const [pm, acc] =
+                await Promise.all([
+                    tx.property_managers.save(property_manager),
+                    tx.accountants.save(accountant),
+                ])
+
+            prop.property_manager_id = pm.id
+            prop.accountant_id = acc.id
+
+            const propRec = await tx.properties.save(prop)
+
+            await tx.declaration_files.destroy({
+                property_id: propRec.id,
+            })
+
+            if (declaration_file)
+                await tx.declaration_files.insert({
+                    property_id: propRec.id,
+                    content: Buffer.from(declaration_file, 'base64'),
+                })
+
+            await tx.buildings.destroy({property_id: propRec.id})
+
+            await Promise.all(
+                buildings.map(b => {
+                    b.property_id = propRec.id
+
+                    b.units.forEach(u => u.building_id = undefined)
+                    // ^for massive deep insert
+
+                    return tx.buildings.insert(b, {
+                        deepInsert: b.units.length > 0,
+                    })
+                }))
+
+            return propRec
+        })
+
+
+module.exports = {
+    ...generateUpsertHandler(true),
+    generateUpsertHandler,
 }
--- backend/routes/updateProperty.js
+++ backend/routes/updateProperty.js
@@ -1,51 +1,6 @@
 const
     {getDB} = require('../db'),
-    upsertProperty = require('../upsertProperty'),
-    {PropertySchema} = require('../../schema')
+    {generateUpsertHandler} = require('./createProperty')
 
 
-module.exports = {
-    url: '/properties/:id',
-    method: 'put',
-
-    schema: {
-        summary: 'Update property',
-        params: {
-            type: 'object',
-            properties: {
-                id: {type: 'integer'},
-            },
-        },
-        body: PropertySchema,
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    id: {type: 'integer'},
-                    property_manager_id: {type: 'integer'},
-                    accountant_id: {type: 'integer'},
-                },
-            },
-        },
-    },
-
-    handler: async (req, rep) => {
-        const db = await getDB()
-
-        try {
-            const propRec =
-                await upsertProperty(db, {
-                    id: req.params.id,
-                    ...req.body,
-                })
-
-            rep.status(200).send(propRec)
-
-        } catch (e) {
-            if (e.code === '23505')
-                rep.status(409).send()
-            else
-                throw e
-        }
-    },
-}
+module.exports = generateUpsertHandler(false)
